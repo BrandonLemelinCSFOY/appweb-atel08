e3f7fc930fee7df3b581e2c7f2ccd8799d993af2
BrandonLemelinCSFOY <2135919@csfoy.ca>
2025-02-18 09:23:48 -0500

    Ajout du workflow

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <img alt="Vue logo" src="../assets/TSbeLike.jpg" />
+  <img alt="Vue logo" src="../assets/images/TSbeLike.jpg" />
 </template>
 
 <style scoped></style>
